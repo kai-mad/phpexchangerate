4c727cb43fa13a888e8ae339f95a3a155841874f
kai <kai@kodebyraaet.no>
2025-03-27 13:47:00 +0100

    add some tests

---
+++ tests/ExchangerateDotHostApiTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace CliExchangeRate\Tests;
+
+use App\Service\ExchangeRateDotHostApi;
+use PHPUnit\Framework\TestCase;
+use Dotenv\Dotenv;
+
+final class ExchangerateDotHostApiTest extends TestCase {
+	public function testApiKeyExists() {
+		$dotenv = Dotenv::createImmutable(dirname(__DIR__, 1));
+		$dotenv->load();
+		self::assertNotEmpty($_ENV["EXCHANGERATE.HOST_API_KEY"]);
+	}
+
+	public function testApiResponseCanCreateExchangeRateObject() {
+		$api = new ExchangeRateDotHostApi();
+		$expectedExchangeRateObject = $api->getRecentExchangeRatesForCurrency("USD");
+		self::assertInstanceOf("App\Model\ExchangeRate", $expectedExchangeRateObject);
+	}
+
+}
